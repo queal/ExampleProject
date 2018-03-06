9b2a6e95153401c82e347582b97738755322a4e1
23820031@qq.com <23820031@qq.com>
2018-03-06 17:09:07 +0800

    add tools`

--- src/com/example/utils/HttpClientUtils.java
+++ src/com/example/utils/HttpClientUtils.java
@@ -2,6 +2,7 @@ package com.example.utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpConnectionManager;
@@ -29,15 +30,17 @@ public class HttpClientUtils {
 	private static final int defaultMaxConnPerHost = 30;
 	private static final int defaultMaxTotalConn = 80;
 
-	/** 默认等待HttpConnectionManager返回连接超时（只有在达到最大连接数时起作用）*/
+	/** 默认等待HttpConnectionManager返回连接超时（只有在达到最大连接数时起作用） */
 	private static final long defaultHttpConnectionManagerTimeout = 3 * 1000;
 
 	private static HttpConnectionManager connectionManager;
 
 	static {
 		connectionManager = new MultiThreadedHttpConnectionManager();
-		connectionManager.getParams().setDefaultMaxConnectionsPerHost(defaultMaxConnPerHost);
-		connectionManager.getParams().setMaxTotalConnections(defaultMaxTotalConn);
+		connectionManager.getParams().setDefaultMaxConnectionsPerHost(
+				defaultMaxConnPerHost);
+		connectionManager.getParams().setMaxTotalConnections(
+				defaultMaxTotalConn);
 
 		IdleConnectionTimeoutThread ict = new IdleConnectionTimeoutThread();
 		ict.addConnectionManager(connectionManager);
@@ -48,13 +51,17 @@ public class HttpClientUtils {
 
 	public static HttpClient getHttpClient() {
 		HttpClient httpClient = new HttpClient(connectionManager);
-		httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(defaultConnectionTimeout);
-		httpClient.getHttpConnectionManager().getParams().setSoTimeout(defaultSoTimeout);
-		httpClient.getParams().setConnectionManagerTimeout(defaultHttpConnectionManagerTimeout);
+		httpClient.getHttpConnectionManager().getParams()
+				.setConnectionTimeout(defaultConnectionTimeout);
+		httpClient.getHttpConnectionManager().getParams()
+				.setSoTimeout(defaultSoTimeout);
+		httpClient.getParams().setConnectionManagerTimeout(
+				defaultHttpConnectionManagerTimeout);
 		return httpClient;
 	}
 
-	public static String doPost(HttpClient httpClient, String url, List<NameValuePair> params) {
+	public static String doPost(HttpClient httpClient, String url,
+			List<NameValuePair> params) {
 		String body = "";
 
 		try {
@@ -78,11 +85,41 @@ public class HttpClientUtils {
 		return body;
 	}
 
-	public static String doPostJson(HttpClient httpClient, String url, String json) {
+	public static String doPost(HttpClient httpClient, String url,
+			Map<String, String> params) {
+		String body = "";
+
+		try {
+			PostMethod httppost = new PostMethod(url);
+			httppost.getParams().setContentCharset("utf-8");
+			if (params != null) {
+				NameValuePair[] p = new NameValuePair[params.size()];
+				int i = 0;
+				for (String key : params.keySet()) {
+					p[i++] = new NameValuePair(key, params.get(key));
+				}
+				httppost.setRequestBody(p);
+			}
+			httpClient.executeMethod(httppost);
+
+			if (httppost.getStatusCode() == HttpStatus.SC_OK) {
+				byte[] bytes = httppost.getResponseBody();
+				body = new String(bytes, "utf-8");
+			}
+			httppost.releaseConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return body;
+	}
+
+	public static String doPostJson(HttpClient httpClient, String url,
+			String json) {
 		String body = "";
 		try {
 			PostMethod httppost = new PostMethod(url);
-			StringRequestEntity entity = new StringRequestEntity(json, "application/json", "utf-8");
+			StringRequestEntity entity = new StringRequestEntity(json,
+					"application/json", "utf-8");
 			httppost.setRequestEntity(entity);
 			httpClient.executeMethod(httppost);
 
@@ -101,7 +138,8 @@ public class HttpClientUtils {
 		String body = "";
 		try {
 			PostMethod httppost = new PostMethod(url);
-			StringRequestEntity entity = new StringRequestEntity(xml, "text/xml", "utf-8");
+			StringRequestEntity entity = new StringRequestEntity(xml,
+					"text/xml", "utf-8");
 			httppost.setRequestEntity(entity);
 			httpClient.executeMethod(httppost);
 
@@ -151,15 +189,16 @@ public class HttpClientUtils {
 		return file;
 	}
 
-	public static String uploadFile(HttpClient httpClient, String url, List<NameValuePair> params, String filename,
-			byte[] data) {
+	public static String uploadFile(HttpClient httpClient, String url,
+			List<NameValuePair> params, String filename, byte[] data) {
 		String body = "";
 		try {
 			PostMethod httppost = new PostMethod(url);
 			httppost.getParams().setContentCharset("utf-8");
 
 			List<Part> parts = new ArrayList<Part>();
-			parts.add(new FilePart(filename, new ByteArrayPartSource(filename, data)));
+			parts.add(new FilePart(filename, new ByteArrayPartSource(filename,
+					data)));
 
 			for (NameValuePair pair : params) {
 				parts.add(new StringPart(pair.getName(), pair.getValue()));
@@ -168,7 +207,8 @@ public class HttpClientUtils {
 			Part[] _parts = new Part[parts.size()];
 			parts.toArray(_parts);
 
-			MultipartRequestEntity entity = new MultipartRequestEntity(_parts, httpClient.getParams());
+			MultipartRequestEntity entity = new MultipartRequestEntity(_parts,
+					httpClient.getParams());
 			httppost.setRequestEntity(entity);
 			httpClient.executeMethod(httppost);
 
